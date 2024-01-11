0d2895ca6d45d1d5e281516c216888f7ec23f569
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-01-11 20:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-function generateRandomCode() {
-	let num = parseInt(Math.random() * 100);
-	if ((48 <= num && num <= 57) || (65 <= num && num <= 90) || (97 <= num && nu <= 122)) {
-		return num;
-	}
-	return 100 + parseInt(num / 5);
+// function generateRandomCode() {
+// 	let num = parseInt(Math.random() * 100);
+// 	if ((48 <= num && num <= 57) || (65 <= num && num <= 90) || (97 <= num && nu <= 122)) {
+// 		return num;
+// 	}
+// 	return 100 + parseInt(num / 5);
+// }
+// function makeid(n) {
+//   // write your code here
+// 	let str = "";
+// 	for (let i = 0; i <= n; i++) {
+// 		str += String.fromCharCode(generateRandomCode());
+// 	}
+// 	return str;
+// }
+
+function makeid(length) {
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    result += charset.charAt(randomIndex);
+  }
+
+  return result;
 }
-function makeid(n) {
-  // write your code here
-	let str = "";
-	for (let i = 0; i <= n; i++) {
-		str += String.fromCharCode(generateRandomCode());
-	}
-	return str;
-}
+
+// Examples
+console.log(makeid(5)); // e.g., "K3f2G"
+console.log(makeid(10)); // e.g., "aB9EfgT2X1"
+
 console.log(makeid(5));
 console.log(makeid(10));;
 // Do not change the code below.
