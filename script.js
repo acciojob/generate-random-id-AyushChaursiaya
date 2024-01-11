826241da3718bde790f6cc57d09b6d95b7cc6a36
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-01-11 20:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function makeid(l) {
-  // write your code here
+function generateRandomCode() {
+	let num = parseInt(Math.random() * 100);
+	if ((48 <= num && num <= 57) || (65 <= num && num <= 90) || (97 <= num && nu <= 122)) {
+		return num;
+	}
+	return 100 + parseInt(num / 5);
 }
-
+function makeid(n) {
+  // write your code here
+	let str = "";
+	for (let i = 0; i < n.length; i++) {
+		str += String.fromCharCode(generateRandomCode());
+	}
+	return str;
+}
+console.log(makeid(n));
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+const n = prompt("Enter a number.");
+alert(makeid(n));
